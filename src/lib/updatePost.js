a2ee865951d0c6224b93422b83122da4fad585d0
Caio César <caiovega7@gmail.com>
2018-11-16 00:05:39 -0200

    feat: atualizando dados filme

---
+++ src/lib/updatePost.js
@@ -0,0 +1,36 @@
+'use strict';
+
+require('./connection');
+const Post = require('../model/post');
+
+function updatePost(res, id, body) {
+  // verificando se é um ObjectId válido
+  if (id.match(/^[0-9a-fA-F]{24}$/)) {
+    delete body._id;
+
+    Post.updateOne({ _id: id }, body, (error, result) => {
+      if (error === null) {
+        let requestResult;
+        if (result.n === 1) {
+          requestResult = {
+            status: 'success',
+            message: 'Atualizado com sucesso!'
+          };
+        } else {
+          requestResult = {
+            status: 'erro',
+            message: 'Não foi possível atualizar, verifique os dados!'
+          };
+        }
+        res.status(200).json(requestResult);
+      } else {
+        console.log(error);
+        res.status(503);
+      }
+    });
+  } else {
+    res.status(200).json({ status: 'erro', message: 'Verifique o ID informado' });
+  }
+}
+
+module.exports = updatePost;
